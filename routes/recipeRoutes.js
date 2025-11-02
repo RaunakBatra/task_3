6836fc61aabc2bd283450c1f42183feed882a50e
[Mohit Mishra] <[mishramohit4905@gmail.com]>
2025-11-02 21:38:47 +0530

    your commit message

--- routes/recipeRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getAllRecipes } from "../controllers/recipeController.js";
-
-const router = express.Router();
-
-// GET /api/recipes
-router.get("/", getAllRecipes);
-
-export default router;
--- routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express"
-import userAuth from "../middleware/usermiddle.js"
-
-import { getUserData } from "../controllers/userController.js"
-const userRouter=express.Router()
-userRouter.get("/data",userAuth,getUserData)
-export default userRouter;
